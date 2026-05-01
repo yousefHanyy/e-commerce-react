15e090a1dca532a0842c2d79256b3ee776520f76
Yousef Hany <116505809+yousefHanyy@users.noreply.github.com>
2026-05-02 01:12:13 +0300

    - ProductForm page: --Added some placeholder start for the form look.

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -11,9 +11,22 @@ function Admin({ items }) {
     <div>
       <NavLink
         to="/product/new"
-        className="btn size-15 text-3xl  text-white bg-green-500 btn-circle fixed bottom-3 right-3"
+        className="btn btn-xl text-white bg-green-500 btn-circle fixed bottom-3 right-3"
       >
-        +
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
       </NavLink>
       <table className="table">
         <thead>
--- src/pages/ProductForm.jsx
+++ src/pages/ProductForm.jsx
@@ -1,5 +1,23 @@
 function ProductForm() {
-  return <div></div>;
+  // Name
+  // Price
+  // Choosing a category from a dropdown list and when chosen assigns a categoryId value.
+  return (
+    <div className="flex justify-center items-center mt-25">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Adding products:</legend>
+
+        <label className="label">Product Name</label>
+        <input type="text" className="input" placeholder="Fries" />
+
+        <label className="label">Product Price</label>
+        <input type="text" className="input" placeholder="75" />
+
+        <label className="label">Author</label>
+        <input type="text" className="input" placeholder="Name" />
+      </fieldset>
+    </div>
+  );
 }
 
 export default ProductForm;
